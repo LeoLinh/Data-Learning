feddee7a84bd09cbb73da1cfb9ef59a846791c17
LeoLinh <nnhatlinhh@gmail.com>
2024-08-21 22:06:24 +0700

    updated on 21.08

--- Assets/Scripts/DailyQuestItemHandler.cs
+++ Assets/Scripts/DailyQuestItemHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class DailyQuestItemHandler : MonoBehaviour
@@ -30,7 +28,6 @@ public class DailyQuestItemHandler : MonoBehaviour
         this.dailyQuestData = dailyQuestData;
         this.QuestProgress = questProgress;
     }
-
     [ContextMenu("Update UI")]
     public void UpdatedUi()
     {
